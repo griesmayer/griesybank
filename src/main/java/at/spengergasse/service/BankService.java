b05202190b27547efd53ea9a0cbb640ffcab75c6
Thomas Griesmayer <griesmayer@spengergasse.at>
2026-04-14 18:01:25 +0200

    Add Service Class

---
+++ src/main/java/at/spengergasse/service/BankService.java
@@ -0,0 +1,50 @@
+package at.spengergasse.service;
+
+import at.spengergasse.domain.Account;
+import com.github.javafaker.Faker;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Service
+public class BankService {
+    private ArrayList<Account> accounts;
+
+    public BankService() {
+        accounts = new ArrayList<>(1000);
+        fillTestData(500);
+    }
+
+    public void fillTestData(int count) {
+        String[] accounttypes = { "Checking", "Savings", "Fixed Deposit", "Business", "Joint" };
+        Account   a;
+        Faker     faker = new Faker();
+        String    firstName;
+        LocalDate openingDate;
+        String    accountType;
+        Double    amount;
+        Boolean   active;
+
+        for (int i=0; i<count; i++) {
+            firstName = faker.address().firstName();
+            openingDate = LocalDate.now().minusDays((int) Math.random() * 3650);
+            accountType = accounttypes[faker.random().nextInt(0, accounttypes.length - 1)];
+            amount = faker.number().randomDouble(2, 0, 50000);
+            active = faker.bool().bool();
+            a = new Account(firstName, openingDate, accountType, amount, active);
+            accounts.add(a);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return accounts.stream()
+                .map(s -> s.toString())
+                .collect(Collectors.joining("\n"));
+    }
+}
---
+++ src/test/java/at/spengergasse/service/BankServiceTest.java
@@ -0,0 +1,14 @@
+package at.spengergasse.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankServiceTest {
+
+    @Test
+    public void testFillTestData() {
+        BankService spar = new BankService();
+        System.out.println(spar);
+    }
+}
